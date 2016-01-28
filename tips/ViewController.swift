d7ee6b826bbb9e6a8ecd15bffb694c0269c690fc
Aditya Balwani <adibalwani@gmail.com>
2016-01-28 10:11:08 -0500

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Aditya Balwani on 1/28/16.
+//  Copyright © 2016 Aditya Balwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
